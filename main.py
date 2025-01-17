0c5350c5052f256db863b963c6c770c54b47181f
Naphat Phoruang <99393112+guide1121@users.noreply.github.com>
2025-01-17 15:41:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, color):
         self.x = x
         self.y = y
+        self.color = color
     def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
+        return Point(self.x + other.x, self.y + other.y, self.color +'+'+ other.color)
     def __sub__(self, other):
-        return Point(self.x - other.x, self.y - other.y)
+        return Point(self.x - other.x, self.y - other.y, self.color +'-'+ other.color)
     def __mul__(self, other):
-        return Point(self.x * other.x, self.y * other.y)
+        return Point(self.x * other.x, self.y * other.y, self.color +'*'+ other.color)
     def __str__(self):
-        return f"Point({self.x}, {self.y})"
+        return f"Point({self.x}, {self.y}, {self.color})"
     
-p1 = Point(2,3)
-p2 = Point(4,5)
+p1 = Point(2,3,"red")
+p2 = Point(4,5,"Blue")
 
-print(p1+p2)
-print(p1-p2)
-print(p1*p2)
-    
+print(f"p1 + p2 = {p1+p2}")
+print(f"p1 - p2 = {p1-p2}")
+print(f"p1 - p2 = {p1*p2}")
+
